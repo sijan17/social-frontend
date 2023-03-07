5f86c6b65465acddb4d1f099f222a4a074801df4
sijan <sijan@localhost.localdomain>
2023-03-07 22:16:49 +0545

    image upload feature

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-// export const host = "http://localhost:5000";
+//export const host = "http://localhost:5000";
 export const host = "https://socialbackend-ukk3.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
