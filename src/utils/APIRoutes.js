5759313ad914325277bb5e1c2b0b422a17ac6d01
sijan <sijan@localhost.localdomain>
2023-03-27 14:18:41 +0545

    updates

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:5000";
-// export const host = "https://socialbackend-ukk3.onrender.com";
+// export const host = "http://localhost:5000";
+export const host = "https://socialbackend-ukk3.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/user/setavatar`;
